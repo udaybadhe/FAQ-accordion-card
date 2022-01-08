6735f2373a7c554f3819f8719db5a02a7bfbee46
Uday Badhe <udaybadhe001@gmail.com>
2022-01-08 22:51:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#FAQ Accordian Card 
+This is a solution to the FAQ Accordion component challenge on Frontend Mentor. Frontend Mentor challenges help you improve your coding skills by building realistic projects.
---
+++ script.js
@@ -0,0 +1,8 @@
+function tog(id) {
+    // document.getElementById(id).setAttribute("class","active");
+    document.getElementById(id).classList.toggle("active");
+    // var ele = document.getElementsByClassName("accordion").value;
+    // ele.bold();
+    // console.log(id.classList.contains("active"));
+
+}
